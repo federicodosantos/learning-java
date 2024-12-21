bdb14ff63cfedf8e1b1203ec6f50c6c30a59cb0c
federico <federicoberto708@gmail.com>
2024-12-21 12:54:03 +0700

    feat : add character, boolean, string data type materials

---
+++ DataType/CharacterDataType.java
@@ -0,0 +1,17 @@
+package DataType;
+
+public class CharacterDataType {
+
+    public static void main(String[] args) {
+        
+        char r = 'R';
+        char i = 'I'; 
+        char k = 'K'; 
+        char o = 'O'; 
+
+        System.out.print(r);
+        System.out.print(i);
+        System.out.print(k);
+        System.out.print(o);
+    }
+}
---
+++ DataType/StringDataType.java
@@ -0,0 +1,13 @@
+package DataType;
+
+public class StringDataType {
+    public static void main(String[] args) {
+        String firstName = "Federico";
+        String lastName = "Dos Santos";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
