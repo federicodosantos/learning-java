a61b98db4b8b53fde63a326de524392fd06ebab9
federico <federicoberto708@gmail.com>
2024-12-21 13:42:22 +0700

    feat : add expression, statement, and block materials

---
+++ Block.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Block {
+    public static void main(String[] args) {
+        double aValue = 8654.432;
+        aValue++;
+        System.out.println("Hello World");
+        Date date = new Date();
+    }
+}
