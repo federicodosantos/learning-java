dd863f47f0aa1f771e523ec57ed528c3265acf46
federico <federicoberto708@gmail.com>
2024-12-21 12:43:58 +0700

    feat : add numeric data type material

---
+++ DataType/NumberDataType.java
@@ -0,0 +1,26 @@
+package DataType;
+
+public class NumberDataType {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.12F;
+        double iniDouble = 12.2424;
+
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b10101010;
+        
+        
+        /*Konversi Tipe data
+        Narrowing casting dari short ke long (otomatis)
+        Widening casting dari long ke short (manual)
+         */
+        
+        
+    }
+}
