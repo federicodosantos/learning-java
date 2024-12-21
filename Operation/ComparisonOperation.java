51420a5e4737c0a78db85710461ef8cb93a2e468
federico <federicoberto708@gmail.com>
2024-12-21 13:26:18 +0700

    feat : add operation materials

---
+++ Operation/ComparisonOperation.java
@@ -0,0 +1,15 @@
+package Operation;
+
+public class ComparisonOperation {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
---
+++ Operation/MathematicOperation.java
@@ -0,0 +1,14 @@
+package Operation;
+
+public class MathematicOperation {
+    public static void main(String[] args) {
+        int a = 50;   
+        int b = 10;
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
